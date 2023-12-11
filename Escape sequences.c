ea2d1de26f0ee0439457ebc961e173515f88133b
Harikeswaran <50329839+Harikeswaran@users.noreply.github.com>
2023-12-11 15:44:54 +0530

    Update Escape sequences.c

--- Escape sequences.c
+++ Escape sequences.c
@@ -1,7 +1,24 @@
+// exercise-1
+
 #include <stdio.h>
 
 int main() 
 {
     printf("David says , \"Programming is fun !\"");
     printf("\nGood day !");
+}
+
+
+// exercide-2
+
+#include <stdio.h>
+
+int main()
+{
+    printf("David says , \"Programming is fun !\"");
+    printf("\n**Conditions apply ,\"Offers valid until tomorrow\"");
+    printf("\nC:\\My computer\\My folder");
+    printf("\nD:/My documents/My file");
+    printf("\n\\\\\\\\ Today is Holiday \\\\\\\\");
+    printf("\nThis is a triple quoted string \"\"\" This month has 30 days \"\"\"");
 }
